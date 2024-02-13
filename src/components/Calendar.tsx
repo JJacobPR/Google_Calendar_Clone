ea6b76a919ffb5da90b5310cc61be431b5a6d586
Jakub Janicki <jjanicki08@gmail.com>
2024-02-13 22:30:56 +0100

    Calendar rendering and navigation

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,82 @@
+import { useMemo, useState } from "react";
+import { startOfWeek, startOfMonth, endOfWeek, endOfMonth, eachDayOfInterval, isSameMonth, isBefore, endOfDay, isToday, subMonths, addMonths } from "date-fns";
+import "./Calendar.css";
+import { formatDate } from "../utils/formatDate";
+import { cc } from "../utils/cc";
+
+const Calendar = () => {
+  const [selectedMonth, setSelectedMonth] = useState(new Date());
+
+  const calendarDays = useMemo(() => {
+    const firstWeekStart = startOfWeek(startOfMonth(selectedMonth));
+    const lastWeekEnd = endOfWeek(endOfMonth(selectedMonth));
+    return eachDayOfInterval({ start: firstWeekStart, end: lastWeekEnd });
+  }, [selectedMonth]);
+
+  return (
+    <div className="calendar">
+      <div className="header">
+        <button className="btn" onClick={() => setSelectedMonth(new Date())}>
+          Today
+        </button>
+        <div>
+          <button
+            onClick={() => {
+              setSelectedMonth((m) => subMonths(m, 1));
+            }}
+            className="month-change-btn"
+          >
+            &lt;
+          </button>
+          <button
+            onClick={() => {
+              setSelectedMonth((m) => addMonths(m, 1));
+            }}
+            className="month-change-btn"
+          >
+            &gt;
+          </button>
+        </div>
+        <span className="month-title">{formatDate(selectedMonth, { month: "long", year: "numeric" })}</span>
+      </div>
+      <div className="days">
+        {calendarDays.map((day, index) => (
+          <CalendarDay key={day.getTime()} day={day} showWeekName={index < 7} selectedMonth={selectedMonth} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+type CalendarDayProps = {
+  day: Date;
+  showWeekName: boolean;
+  selectedMonth: Date;
+};
+
+const CalendarDay = ({ day, showWeekName, selectedMonth }: CalendarDayProps) => {
+  return (
+    <div className={cc("day", isSameMonth(day, selectedMonth) && "non-month-day", isBefore(endOfDay(day), new Date()) && "old-month-day")}>
+      <div className="day-header">
+        {showWeekName && <div className="week-name">{formatDate(day, { weekday: "short" })}</div>}
+        <div className={cc("day-number", isToday(day) && "today")}>{formatDate(day, { day: "numeric" })}</div>
+        <button className="add-event-btn">+</button>
+      </div>
+      {/* <div className="events">
+        <button className="all-day-event blue event">
+          <div className="event-name">Short</div>
+        </button>
+        <button className="all-day-event green event">
+          <div className="event-name">Long Event Name That Just Keeps Going</div>
+        </button>
+        <button className="event">
+          <div className="color-dot blue"></div>
+          <div className="event-time">7am</div>
+          <div className="event-name">Event Name</div>
+        </button>
+      </div> */}
+    </div>
+  );
+};
+
+export default Calendar;
